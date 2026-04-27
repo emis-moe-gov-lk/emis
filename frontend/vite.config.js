068d45e726391dafac015dfad73db9e7536ac615
Roshan Dissanayake <hasantharoshan22@gmail.com>
2026-04-27 14:42:51 +0530

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
-//import { createRequire } from "module";
 
-//const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
