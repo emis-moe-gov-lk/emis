0f30a4af939cf1bdfc1b006b5b6e007c9f172e09
gavin gunapala <gavingunapala06@gmail.com>
2026-05-08 09:43:54 +0530

    ED-97 Create a princial role

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -128,6 +128,8 @@ Route::prefix('')->group(function () {
 
     Route::controller(PrincipalApiController::class)->middleware('auth:jwt')->group(function () {
         Route::get('/principals-list', 'principalList');  // GET all principals
+        Route::post('/principal-create', 'store');
+        Route::get('/principal-recruitment-categories', 'recruitmentCategories');
         Route::get('/principal/{people_id}', 'getPrincipal');
     });
 
@@ -145,6 +147,7 @@ Route::prefix('')->group(function () {
         Route::patch('/employer-appointment-reject-comments/{id}', 'updateRejectComment');
         Route::patch('/teachers/{people_id}/verify', 'verify');
         Route::patch('/teachers/{people_id}/confirm', 'confirm');
+        Route::patch('/principals/{people_id}/confirm', 'confirm');
         Route::patch('/teachers/{people_id}/promote', 'promote');
         Route::patch('/teachers/{people_id}/reject', 'reject');
         Route::patch('/teachers/{people_id}/update', 'updateRejectedStatus');
